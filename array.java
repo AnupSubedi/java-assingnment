506578c1e2899459f50ecd1691660f3233fe4555
Anoop <anupsubedi87@gmail.com>
2020-02-11 17:18:41 +0545

    Succes

---
+++ array.java
@@ -0,0 +1,14 @@
+class array{ 
+            public static void main(String [] args){
+                   String name[];
+                   name= new String[5];
+                   name[0]="Anurag";
+                  name[1]="Anup";
+                  name[2]="Arjun";
+                  name[3]="Aadarsha";
+                 name[4]="Badarsha";
+        for(i=0; i<5; i++){
+             System.out.println(name[i])
+}
+}
+}
